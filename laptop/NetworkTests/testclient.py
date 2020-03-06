62315a922ef7dad8d86480bf4df08370e30e1d2c
zach-lau <zlau2000@gmail.com>
2020-03-06 13:22:29 -0700

    removed test file

--- laptop/NetworkTests/testclient.py
+++
@@ -1,7 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.1.67', 5005))
-s.sendall("Hello, world")
-data = s.recv(1024) #receives 1024 bytes
-s.close()
-print("Received %s" % data)
