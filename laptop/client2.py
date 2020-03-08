f7b9c180cf514a40400197824d5c3c7c8895f362
zach-lau <zlau2000@gmail.com>
2020-03-08 14:13:49 -0600

    Created motorcommand struct

--- laptop/client2.py
+++ laptop/client2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import tkinter as tk
 import socket
+from motorcommands import *
 
 SERVER_ADDRESS = "192.168.1.65"
 
@@ -17,12 +18,43 @@ def connect(server_address):
 		print("Error making socket")
 		return None
 
+def parseInput(line): 
+	# Turns a users command line input into an appropriate integer output
+	# If the command is not valid it will output 0 and notify the user of an unrecognized command
+
+	if line == "forward": 
+		left = 255
+		right = 255
+	elif line == "right":
+		left = -255
+		right = 255
+	elif line == "left":
+		left = 255
+		right = -255
+	elif line == "back": 
+		left = -255
+		right = -255
+	else: 
+		left = 0
+		right = 0
+		print("Unrecognized command")
+	return (left, right)
+
 def cli(sock):
 	# A command line interface that talks with the server
 	# sock : socket to communicate with 
 	line = input()
+	# left : left motor pwm from -255 to 255
+	# right : right motor pwm from -255 to 255
+
 	while line:
-		sock.sendall(bytes(line, 'utf-8'))
+		left, right = parseInput(line)
+		command = motor_command(left, right)
+		try: 
+			command.send(sock)
+		except:
+			print("Unable to send command")
+		#sock.sendall(bytes(line, 'utf-8'))
 		line = str(input())
 	sock.close()
 	print("Closing client")
