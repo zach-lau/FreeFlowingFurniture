792b7afc7904ad4f66d5599388d6702549be04de
zach-lau <zlau2000@gmail.com>
2020-03-08 14:21:47 -0600

    Sending five things at once

--- laptop/motorcommands.py
+++ laptop/motorcommands.py
@@ -2,6 +2,7 @@ import socket
 import struct
 
 class motor_command: 
+
     def __init__(self, left = 0, right = 0, heartbeat = 1):
         self._heartbeat = heartbeat
         self._lefta = max(0, left)
@@ -10,5 +11,5 @@ class motor_command:
         self._rightb = max(0, -right)
 
     def send(self, socket): 
-        print("Trying to send...")
-        socket.sendall(struct.pack('!ii', self._lefta, self._leftb)) #, self._righta, self._rightb))
+        print("Send command...")
+        socket.sendall(struct.pack("!iiiii", self._heartbeat, self._lefta, self._leftb, self._righta, self._rightb))
