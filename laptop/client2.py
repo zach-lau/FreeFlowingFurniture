6dbf99ffc3f3c5f373eded181ed5a33907690ed6
zach-lau <zlau2000@gmail.com>
2020-03-08 11:13:15 -0600

    Made socket function

--- laptop/client2.py
+++ laptop/client2.py
@@ -1,10 +1,32 @@
+#!/usr/bin/python3
+import tkinter as tk
 import socket
+
 SERVER_ADDRESS = "192.168.1.65"
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_ADDRESS, 5005))
-line = input()
-while line:
-	s.sendall(bytes(line, 'utf-8'))
-	line = str(input())
-s.close()
-print("Closing client");
+
+def connect(server_address):
+	# Connect to the server at the given address
+	# server_address : local ip of the server
+	# return : The socket if successful, None if could not connect 
+	try:
+		#Connect to the pi 
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((server_address, 5005))
+		return s
+	except: 
+		return None
+
+def main(): 
+	s = connect(SERVER_ADDRESS)
+	if s == None:
+		return 
+	line = input()
+	while line:
+		s.sendall(bytes(line, 'utf-8'))
+		line = str(input())
+	s.close()
+	print("Closing client")
+
+
+if __name__ == '__main___':
+	main()
